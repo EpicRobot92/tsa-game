1e3e5e83ebcc07f76fdeca0d64b4ef8d20147bec
EpicRobot9 <epicrobot20099@gmail.com>
2026-01-18 12:57:23 -0500

    Swap Mechanic DONE
    Added twin swap mechanic with independent animation players for Nova and Ecliptio

--- scenes/Characters/player.gd
+++ scenes/Characters/player.gd
@@ -3,13 +3,56 @@ extends Character
 
 @onready var enemy_slots: Array = $EnemySlots.get_children()
 
+enum Twin { ECLIPTIO, NOVA }
+
+@onready var ecliptio_visual: Node2D = $EcliptioVisual
+@onready var nova_visual = $NovaVisual
+@export var swap_cooldown := 500
+
+var current_twin := Twin.ECLIPTIO
+var active_visual: Node2D
+
+var time_since_swapped = Time.get_ticks_msec()
+
+
+
+
+
+
 func _ready() -> void:
 	super._ready()
 	anim_attacks = ["punch", "punch_alt", "kick", "roundkick"]
+	## sets up the current twin
+	set_active_visual(
+		nova_visual if current_twin == Twin.NOVA else ecliptio_visual
+	)
+	
+## Makes the current twin visible
+func set_active_visual(v: Node2D) -> void:
+	if active_visual:
+		active_visual.visible = false
+
+	active_visual = v
+	active_visual.visible = true
+
+	animation_player = active_visual.get_node("AnimationPlayer")
+	character_sprite = active_visual.get_node("CharacterSprite")
+	
+## swaps the twin and plays the current state
+func swap_twin():
+	current_twin = Twin.NOVA if current_twin == Twin.ECLIPTIO else Twin.ECLIPTIO
+	set_active_visual(
+		nova_visual if current_twin == Twin.NOVA else ecliptio_visual
+	)
+	animation_player.play(anim_map[state])
+## checks if the player can swap by getting the time in-tween last swap and checks if more or equal time has passed since the cooldown and if the state is idle
+func can_swap() -> bool:
+	return true if (Time.get_ticks_msec() - time_since_swapped >= swap_cooldown) and [State.IDLE].has(state) else false
+
 
 
 func handle_input() -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 
 	if can_attack() and Input.is_action_just_pressed("attack"):
@@ -24,6 +67,10 @@ func handle_input() -> void:
 
 	if can_jumpkick() and Input.is_action_just_pressed("attack"):
 		state = State.JUMPKICK
+	if can_swap() and Input.is_action_just_pressed("swap"): 
+		swap_twin()
+		time_since_swapped = Time.get_ticks_msec()
+	
 		
 func set_heading() -> void: 
 	if velocity.x > 0: 
